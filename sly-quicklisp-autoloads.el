06d7281e70d71b9a37f488c8f63fd199e1fb0f97
R Primus <rprimus@users.noreply.github.com>
2019-07-01 15:44:10 +0100

    * sly-quicklisp-autoloads.el: Generate using package-generate-autoloads

--- sly-quicklisp-autoloads.el
+++ sly-quicklisp-autoloads.el
@@ -1,10 +1,31 @@
+;;; sly-quicklisp-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
-;;;### (autoloads nil "sly-quicklisp" "sly-quicklisp.el"
-;;;;;;  (21964 37122 0 0))
+;;;### (autoloads nil "sly-quicklisp" "sly-quicklisp.el" (0 0 0 0))
 ;;; Generated autoloads from sly-quicklisp.el
 
 (add-to-list 'sly-contribs 'sly-quicklisp 'append)
 
-;;;***
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "sly-quicklisp" '("sly-quickl")))
 
-(provide 'sly-quicklisp-autoloads)
+;;;***
+
+;;;### (autoloads nil "sly-quicklisp-tests" "sly-quicklisp-tests.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from sly-quicklisp-tests.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "sly-quicklisp-tests" '("sly-quicklisp-basic-test")))
+
+;;;***
+
+;; Local Variables:
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; sly-quicklisp-autoloads.el ends here
