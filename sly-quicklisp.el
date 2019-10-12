44a8ac840381d9015281527eb13fefc1a9a57fce
Pierre Neidhardt <mail@ambrevar.xyz>
2019-10-12 19:52:41 +0200

    Actually explain what sly-quicklisp is in commentary.

--- sly-quicklisp.el
+++ sly-quicklisp.el
@@ -22,10 +22,10 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;;
 ;;; Commentary:
-;; 
-;; `sly-quicklisp` is an external contrib for SLY that does nothing
-;; special, but acts like a template for writing other external
-;; contribs.
+;;
+;; `sly-quicklisp' is an external contrib for SLY that provides a
+;; `sly-quickload' command (C-c C-d C-q) that prompts the user for a package to
+;; install.
 ;;
 ;; See README.md
 ;; 
